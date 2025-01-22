6352742a300c6d8bf07fef10beabc1068155188e
marcelolucaslls <100415381+marcelolucaslls@users.noreply.github.com>
2025-01-21 21:40:38 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import { setupNavigation } from './ui.js';
-import { setupLoginForm } from './form.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    setupNavigation();
-    setupLoginForm();
-});
